da6f252fc39630d2070c77f5d71a2c4f1dd5846c
tomas aronna san martin <tomasaronna@gmail.com>
2024-10-03 15:14:08 -0300

    cree las cards en JS

--- js/main.js
+++ js/main.js
@@ -1,68 +1,102 @@
-const modal = document.querySelector('.modal')
-const close = document.querySelector('.close')
-
-const abrirModal = () => {modal.style.display = "block"}
-const cerrarModal = () => {modal.style.display = "none"}
-
 let razas = [
     {
         raza: "humano",
+        img: "../assets/images/humano.webp",
         atributos: {
             fuerza: 11,
             destreza: 11,
             constitucion: 11,
             inteligencia: 11,
             sabiduria: 11,
-            Carisma: 11
+            carisma: 11
         }
     },
     {
         raza: "tiefling",
+        img: "../assets/images/tiefling.webp",
         atributos: {
             fuerza: 10,
             destreza: 12,
             constitucion: 10,
             inteligencia: 10,
             sabiduria: 10,
-            Carisma: 12,
+            carisma: 12,
         }
     },
     {
         raza: "draconido",
+        img: "../assets/images/draconido.webp",
         atributos: {
             fuerza: 12,
             destreza: 10,
             constitucion: 10,
             inteligencia: 10,
             sabiduria: 10,
-            Carisma: 11,
+            carisma: 11,
         }
     },
     {
         raza: "elfo",
+        img: "../assets/images/elfo.webp",
         atributos: {
             fuerza: 10,
             destreza: 12,
             constitucion: 10,
             inteligencia: 11,
             sabiduria: 11,
-            Carisma: 11,
+            carisma: 11,
         }
     },
     {
         raza: "semiorco",
+        img: "../assets/images/semiorco.webp",
         atributos: {
             fuerza: 12,
             destreza: 10,
             constitucion: 11,
             inteligencia: 10,
             sabiduria: 10,
-            Carisma: 10,
+            carisma: 10,
         }
     }
 ]
 
 const clases = ['guerrero', 'mago', 'brujo', 'explorador']
+
+const modal = document.querySelector('.modal')
+const close = document.querySelector('.close')
+const start = document.querySelector('#start')
+
+let modalContent
+razas.forEach(raza => {
+    modalContent += `
+        <div class="card">
+            <img src="${raza.img}"
+            <h2>${raza.raza}</h2>
+            <h3>Atributos:</h3>
+            <ul>
+              <li>Fuerza: ${raza.atributos.fuerza}</li>
+              <li>Destreza: ${raza.atributos.destreza}</li>
+              <li>Constitución: ${raza.atributos.constitucion}</li>
+              <li>inteligencia: ${raza.atributos.inteligencia}</li>
+              <li>Sabiduría: ${raza.atributos.sabiduria}</li>
+              <li>Carisma: ${raza.atributos.carisma}</li>
+            </ul>
+            <button type="button" class="raceButton" data-raza="${raza.raza}">Elegir raza</button>
+        </div>
+    `
+    modal.innerHTML = `
+        <div class="modalContent">
+            <span class="close">&times</span>
+            ${modalContent}
+        </div>
+    `
+});
+
+start.addEventListener('click', () => {
+    modal.style.display = "block"
+
+})
 
 function elegirPersonaje(race) {
     for (let i = 0; i < razas.length; i++) {
@@ -72,9 +106,9 @@ function elegirPersonaje(race) {
     }
 }
 
-const elegirClase = function() {
-    
-}
+// const elegirClase = function () {
+
+// }
 
 function crearPersonaje(race) {
     const razaSeleccionada = elegirPersonaje(race)
